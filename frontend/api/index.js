090ecdcb214bd7277a411f624935d746e16f2e3c
Develop-KIM <kimdonghwan913@gmail.com>
2025-09-25 16:44:11 +0900

    test: test:

--- frontend/api/index.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";  
-
-const config = {
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-}
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,28 @@
-<script setup></script>
-
 <template>
-  <div>
-    <span>CSAT</span>
-
-    <span>오늘 부터 시작!</span>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<style scoped></style>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
