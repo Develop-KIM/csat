20c0e20d7b20c139eb290faf07bf493f4ad9518b
Kimdonghwan <kimdonghwan913@gmail.com>
2025-09-26 00:36:28 +0900

    test: 리뷰 자동 테스트

--- backend/config/kiwoom.js
+++ backend/config/kiwoom.js
@@ -1,6 +1,6 @@
 module.exports = {
-  appKey: process.env.KIWOOM_API_KEY,
-  appSecret: process.env.KIWOOM_API_SECRET,
+  apiKey: process.env.KIWOOM_API_KEY,
+  apiSecret: process.env.KIWOOM_API_SECRET,
   userId: process.env.KIWOOM_USER_ID,
   password: process.env.KIWOOM_PASSWORD,
   certPassword: process.env.KIWOOM_CERT_PASSWORD,
