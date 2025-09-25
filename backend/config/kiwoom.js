4e430be77d925ba7d3dd1b92217f774e64ac5582
Kimdonghwan <kimdonghwan913@gmail.com>
2025-09-26 00:31:44 +0900

    feat: add Kiwoom API configuration module

---
+++ backend/config/kiwoom.js
@@ -0,0 +1,7 @@
+module.exports = {
+  appKey: process.env.KIWOOM_API_KEY,
+  appSecret: process.env.KIWOOM_API_SECRET,
+  userId: process.env.KIWOOM_USER_ID,
+  password: process.env.KIWOOM_PASSWORD,
+  certPassword: process.env.KIWOOM_CERT_PASSWORD,
+};
