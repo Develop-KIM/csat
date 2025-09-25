f05ca168dc1cf3e984a551739ce33e3287689f87
Kimdonghwan <kimdonghwan913@gmail.com>
2025-09-26 00:38:16 +0900

    chore: 모듈 수정

--- backend/config/kiwoom.js
+++ backend/config/kiwoom.js
@@ -3,6 +3,5 @@ module.exports = {
   apiSecret: process.env.KIWOOM_API_SECRET,
   userId: process.env.KIWOOM_USER_ID,
   password: process.env.KIWOOM_PASSWORD,
-
   certPassword: process.env.KIWOOM_CERT_PASSWORD,
 };
