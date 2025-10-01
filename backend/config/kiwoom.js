a02c46f2115299449e18a4358a19659f1ad018dd
Develop-KIM <kimdonghwan913@gmail.com>
2025-10-01 13:42:25 +0900

    refactor: 환경별 도메인 주소 변경되게 수정

--- backend/config/kiwoom.js
+++ backend/config/kiwoom.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 
+const environment = {
+  development: 'https://mockapi.kiwoom.com',
+  production: 'https://api.kiwoom.com',
+}
+
 module.exports = {
   apiKey: process.env.KIWOOM_API_KEY,
   apiSecret: process.env.KIWOOM_API_SECRET,
+  baseUrl: environment[process.env.NODE_ENV],
 };
